f1e11b33493bf1f546423546d1a6a3b3b88f9f70
lucky98622 <745478424@qq.com>
2019-05-14 18:04:11 +0800

    根启动参数

--- config/config.go
+++ config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"fmt"
 	"github.com/pelletier/go-toml"
+	"os"
 )
 
 var (
@@ -10,7 +11,15 @@ var (
 )
 
 func New() *toml.Tree {
-	config, err := toml.LoadFile("./config/config.toml")
+	var url string
+	if len(os.Args) > 1 && os.Args[1] == "production" {
+		fmt.Println(os.Args)
+		url = "/home/www/ccm/config/config.toml"
+	} else {
+		url = "./config/config.toml"
+	}
+	fmt.Println(url)
+	config, err := toml.LoadFile(url)
 	if err != nil {
 		fmt.Println("Toml Loading Error", err.Error())
 	}
